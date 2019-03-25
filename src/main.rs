8d750d1249a82d817907d96fdd777d774501fbca
Jesse Malinosky <jesse@malinoskj2.dev>
2019-03-24 23:27:21 -0400

    Add structopt dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Error;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+type DIResult<T> = Result<T, Box<Error>>;
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    /// Files to process
+    #[structopt(name = "", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
+fn main() -> DIResult<()> {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    Ok(())
 }
