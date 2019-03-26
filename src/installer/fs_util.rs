8a562196c39ae765139ee613ea13e54a3f88d09b
Jesse Malinosky <jesse@malinoskj2.dev>
2019-03-25 21:57:10 -0400

    remove junk test

--- src/installer/fs_util.rs
+++ src/installer/fs_util.rs
@@ -14,22 +14,3 @@ pub fn find_file_in_dir(dir_path: impl AsRef<Path>, comparator: Vec<String>) -> 
         .map(DirEntry::into_path)
         .collect()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn find_cfg() {
-        let expected = PathBuf::from("/usr/home/jesse/env/package/cargo.yaml");
-
-        let found_paths = find_file_in_dir(
-            PathBuf::from("/usr/home/jesse/env"),
-            vec![String::from("cargo.yaml")],
-        );
-
-        found_paths.iter().for_each(|path| {
-            assert_eq!(&expected, path);
-        });
-    }
-}
