d3b7f41c8d853ef0ad040a2a7798ea9ba86c8730
Jesse Malinosky <jesse@malinoskj2.dev>
2019-06-07 20:05:21 -0400

    add colored help

--- src/lib/option.rs
+++ src/lib/option.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "tbd", about = "make sym links easily.", author = "")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
 pub struct Opt {
     #[structopt(short = "c", long = "config", default_value = "links.yml")]
     pub config_files: Vec<PathBuf>,
